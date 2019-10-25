4503ac4d479d45ec29c2520cc001166fbd5ef1f3
unknown <1710342458@qq.com>
2019-10-25 21:13:20 +0800

    21

---
+++ build/precache-manifest.224e358ff5fbecdded9b7d9432f08b96.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "2df2f3f48ee41dad3a9eac43d304c2b3",
+    "url": "./index.html"
+  },
+  {
+    "revision": "3be4e8c3a0a66e1f9756",
+    "url": "./static/css/main.498d002a.chunk.css"
+  },
+  {
+    "revision": "f86ae8d393774fc707d1",
+    "url": "./static/js/2.79d90b9b.chunk.js"
+  },
+  {
+    "revision": "3be4e8c3a0a66e1f9756",
+    "url": "./static/js/main.9ce91fed.chunk.js"
+  },
+  {
+    "revision": "cddbd24dfe836af852d1",
+    "url": "./static/js/runtime-main.00571ad9.js"
+  }
+]);
--- build/service-worker.js
+++ build/service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "./precache-manifest.d0bad775e0974a1e13ae772ae1600156.js"
+  "./precache-manifest.224e358ff5fbecdded9b7d9432f08b96.js"
 );
 
 self.addEventListener('message', (event) => {
